7db5e6e711f9858a5783db850943a096c019ab21
parvej <parvej1114@yahoo.com>
2020-02-15 22:19:56 +0800

    Solve the PoolPuzzleOne

--- src/head_fast_java/ch1/Test1.java
+++ src/head_fast_java/ch1/Test1.java
@@ -2,6 +2,12 @@ package head_fast_java.ch1;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("we are testing a program form head fast java book.");
+        int x = 0;
+        int y = 0;
+        while (x<5){
+            y = x - y;
+            System.out.println(x + "" + y + " ");
+            x++;
+        }
     }
 }
