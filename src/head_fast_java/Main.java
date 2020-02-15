75ac474c1803f4ea4bc0bcaf77553c74066b1c4f
parvej <parvej1114@yahoo.com>
2020-02-15 21:40:34 +0800

    Chapter one first example program.

---
+++ src/head_fast_java/Main.java
@@ -0,0 +1,8 @@
+package head_fast_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/head_fast_java/ch1/Test1.java
@@ -0,0 +1,7 @@
+package head_fast_java.ch1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("we are testing a program form head fast java book.");
+    }
+}
