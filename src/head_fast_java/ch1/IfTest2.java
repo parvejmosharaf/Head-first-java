5499e710831178bea5bcd17fa98ecc067acf6a1d
parvej <parvej1114@yahoo.com>
2020-03-16 09:17:56 +0800

    added IfTest2

---
+++ src/head_fast_java/ch1/IfTest2.java
@@ -0,0 +1,12 @@
+package head_fast_java.ch1;
+
+public class IfTest2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if(x == 3)
+            System.out.println("x must be 3");
+        else
+            System.out.println("x is not 3.");
+        System.out.println("This is run no matter what.");
+    }
+}
