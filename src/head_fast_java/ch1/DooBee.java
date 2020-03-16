7598941d7c437d2ac617560a9a9d8d15588699d2
parvej <parvej1114@yahoo.com>
2020-03-16 09:22:51 +0800

    solve DooBee

---
+++ src/head_fast_java/ch1/DooBee.java
@@ -0,0 +1,14 @@
+package head_fast_java.ch1;
+
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x<3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        if(x == 3)
+            System.out.println("Do");
+    }
+}
