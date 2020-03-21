6d0bb2a990837c0aff9b43417fdbc29bb058b737
parvej <parvej1114@yahoo.com>
2020-03-21 15:25:09 +0800

    PhraseOMatic edited

--- src/head_fast_java/ch1/PhraseOMatic.java
+++ src/head_fast_java/ch1/PhraseOMatic.java
@@ -12,13 +12,10 @@ public class PhraseOMatic {
         String[] wordListThree = {"process", "tipping-point", "solution", "architecture",
                 "core competency", "strategy", "mindshare", "portal", "space", "vision", "paradigm", "mission"};
 
-        int oneLength = wordListOne.length;
-        int twoLength = wordListTwo.length;
-        int threeLength = wordListThree.length;
 
-        int rand1 = (int) (Math.random() * oneLength);
-        int rand2 = (int) (Math.random() * twoLength);
-        int rand3 = (int) (Math.random() * threeLength);
+        int rand1 = (int) (Math.random() * (wordListOne.length) );
+        int rand2 = (int) (Math.random() * (wordListTwo.length));
+        int rand3 = (int) (Math.random() * (wordListThree.length));
 
         String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
         System.out.println("What we need is a " + phrase);
